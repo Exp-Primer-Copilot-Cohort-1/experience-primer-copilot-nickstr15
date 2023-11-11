a85c3570caff74b62470303c9a7641b93d137272
Nick Striebel <77573839+nickstr15@users.noreply.github.com>
2023-11-11 11:16:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,56 +1,69 @@
-// Create a web server
-// 1. /comments/new
-// 2. /comments/delete
-// 3. /comments/edit
+// Create web server with express
+// Load modules
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+const path = require('path')
 
-// 1. /comments/new
-// 2. /comments/delete
-// 3. /comments/edit
+// Load modules
+const comments = require('./comments.js')
 
-const express = require('express');
-const router = express.Router();
-const Comment = require('../models/comment');
+// Use body parser
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// 1. /comments/new
-router.post('/new', (req, res) => {
-    // Get the data from the form
-    // Create a new comment
-    // Redirect to the home page
-    Comment.create(req.body, (err, comment) => {
-        if(err) {
-            console.log(err);
-        } else {
-            res.redirect('/');
-        }
-    });
-});
+// Set view engine
+app.set('view engine', 'ejs')
 
-// 2. /comments/delete
-router.delete('/delete/:id', (req, res) => {
-    // Get the id from the url
-    // Delete the comment with the given id
-    // Redirect to the home page
-    Comment.findByIdAndRemove(req.params.id, (err) => {
-        if(err) {
-            console.log(err);
-        } else {
-            res.redirect('/');
-        }
-    });
-});
+// Set view folder
+app.set('views', path.join(__dirname, 'views'))
 
-// 3. /comments/edit
-router.put('/edit/:id', (req, res) => {
-    // Get the id from the url
-    // Update the comment with the given id
-    // Redirect to the home page
-    Comment.findByIdAndUpdate(req.params.id, req.body, (err) => {
-        if(err) {
-            console.log(err);
-        } else {
-            res.redirect('/');
-        }
-    });
-});
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
-module.exports = router;
+// Render index page
+app.get('/', (req, res) => {
+  res.render('index', { comments: comments })
+})
+
+// Render new page
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+
+// Create new comment
+app.post('/new', (req, res) => {
+  const newComment = {
+    id: comments.length + 1,
+    name: req.body.name,
+    content: req.body.content
+  }
+  comments.push(newComment)
+  res.redirect('/')
+})
+
+// Render edit page
+app.get('/:id/edit', (req, res) => {
+  const comment = comments.find(comment => comment.id.toString() === req.params.id)
+  res.render('edit', { comment: comment })
+})
+
+// Edit comment
+app.post('/:id/edit', (req, res) => {
+  const comment = comments.find(comment => comment.id.toString() === req.params.id)
+  comment.name = req.body.name
+  comment.content = req.body.content
+  res.redirect('/')
+})
+
+// Delete comment
+app.post('/:id/delete', (req, res) => {
+  const index = comments.findIndex(comment => comment.id.toString() === req.params.id)
+  comments.splice(index, 1)
+  res.redirect('/')
+})
+
+// Listen server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
